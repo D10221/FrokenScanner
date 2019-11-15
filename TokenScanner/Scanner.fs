5ade91c9a73358095a35b0a676ab7b720f6ab83e
Mr D <d10221@protonmail.com>
2019-11-15 15:07:00 +0200

    remove dead code

--- TokenScanner/Scanner.fs
+++ TokenScanner/Scanner.fs
@@ -8,25 +8,22 @@ let isWord (o: Option<char>) = o <> None && Regex("\w+").IsMatch(o.Value.ToStrin
 let isDigit (o: Option<char>) = o <> None && Regex("\d+").IsMatch(o.Value.ToString())
 let isSpace (o: Option<char>) = o <> None && Regex("\s+").IsMatch(o.Value.ToString())
 
-let Result(list: List<Option<char>>) = list |> (List.fold (fun acc o -> acc + o.Value.ToString()) "")
-// |> (List.map (fun o -> o.Value))
-
 ///<summary>
 ///
 ///<summary>
 let rec Scanner (subscriber: List<Option<char>> -> unit) (_next: Next) (x: Option<char>) =
     let scan = fun () -> Scanner subscriber _next (_next (true))
-    let peek = fun () -> 
-        _next (false) // don't advance & return next 
-    let next = fun () ->
-         _next (true) // advance & return current
-    match x with 
+    let peek = fun () -> _next (false) // don't advance & return next
+    let next = fun () -> _next (true) // advance & return current
+    match x with
     | Some('=') ->
         match peek() with
         | Some('=')
         | Some('>') ->
             ignore <| next()
-            subscriber ([ x; next() ])
+            subscriber
+                ([ x
+                   next() ])
         | _ -> subscriber ([ x ])
         // ... finally
         ignore <| scan()
@@ -81,25 +78,27 @@ let rec Scanner (subscriber: List<Option<char>> -> unit) (_next: Next) (x: Optio
 /// <summary>
 /// Starts the Scanner
 /// </summary>
-let Starter(next: Next) = (next, next(true))
+let Starter(next: Next) = (next, next (true))
 
 /// <summary>
 /// Scans ...
 /// </summary>
 let Scan(input: string) =
-    let mutable out: List<string> = []
-    let append chars = out <- out @ [ chars ]
+
+    let mutable accumulator: List<string> = []
+
+    let append chars = accumulator <- accumulator @ [ chars ]
 
     let subscriber (options: List<Option<char>>) =
         options
         |> (List.fold (fun acc o -> acc + o.Value.ToString()) "")
         |> append
-    
+
     let scan = Scanner subscriber
 
     input
     |> (Queue)
     |> Starter
     ||> scan
-    out
+    accumulator
 // |> List.map(fun list-> List.fold(fun a-> fun b -> a + b.ToString()) "" list )
