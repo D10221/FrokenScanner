58ecd5cbfba9fa1d08f87c415841844b57ecbf78
Daniel <danieliozzi@gmail.com>
2019-11-16 09:08:06 +0200

    clean

--- TokenScanner/Queue.fs
+++ TokenScanner/Queue.fs
@@ -4,27 +4,23 @@ open Types
 ///<summary>
 /// Creeates Peek And Next hold index state
 ///</summary>
-let Queue: Queue =
-    fun (input: string) ->
-        printf "queue: %s\n" input
-        let mutable index = -1
-        // ...
-        let ok = fun () -> (index + 1) < (input.Length)
-        // ...
-        let peek unit =
-            if ok() then Some(input.[index + 1])
-            else None
-        // ...
-        let fwd unit =
-                if ok() then
-                    index <- index + 1
-                    Some(input.[index])
-                else
-                    None            
-        // ...
-        let next: Next =
-            fun consume ->
-                if consume then fwd()
-                else peek()
-        // ...
-        next
+let Queue(input: string): bool -> char option =
+    let mutable index = -1
+    let ok unit = (index + 1) < (input.Length)
+
+    let peek unit =
+        if ok() then Some(input.[index + 1])
+        else None
+
+    let fwd unit =
+        if ok() then
+            index <- index + 1
+            Some(input.[index])
+        else
+            None
+
+    let next consume =
+        if consume then fwd()
+        else peek()
+    // ...
+    next
