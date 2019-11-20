c91353e112d8ac9183abacf4e36c6cd8aa3cad93
Mr D <d10221@protonmail.com>
2019-11-20 10:40:19 +0200

    remove unused

--- TokenScanner/Matching.fs
+++ TokenScanner/Matching.fs
@@ -7,13 +7,11 @@ let checkValue check (o: char option) =
         | None -> false
         | some -> check some.Value
 
-let Not (f: 'a -> bool) c = not <| f (c)
+let Not f c = not <| f (c)
 
 let And a b x = a (x) && b (x)
 
 let Or a b x = a (x) || b (x)
-
-let equals x y = x = y
 
 let toString (c: char) = c.ToString()
 
