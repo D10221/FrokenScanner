d2b64ad4a3c7261165a7aefed22d99102dd6f94f
Mr D <d10221@protonmail.com>
2019-12-06 08:35:17 +0200

    fix script

--- MiniParser/parse.fsx
+++ MiniParser/parse.fsx
@@ -1,13 +1,14 @@
 #!/usr/bin/env fsi
 (* in linux/osx make fsi.exe an alias or a link of fsharpi *)
-
-#load "./MiniParser.fs"
+#load "Lexing.fs"
+#load "./Parsing.fs"
+#load "./Parse.fs"
 
 open MiniParser
 open System
 
 let parsed = [
     for x in (Environment.GetCommandLineArgs() |> List.ofArray |> List.skip 3) do
-    yield MiniParser.parseString x ]
+    yield MiniParser.Parse.ParseString x ]
 
 parsed |> printfn "%A\n"
