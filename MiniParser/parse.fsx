0d29f7260ac07236dea15011d0a51ca11d55c4ad
Daniel <danieliozzi@gmail.com>
2019-12-07 05:55:07 +0200

    Script: #r instead of #load

--- MiniParser/parse.fsx
+++ MiniParser/parse.fsx
@@ -1,14 +1,11 @@
 #!/usr/bin/env fsi
 (* in linux/osx make fsi.exe an alias or a link of fsharpi *)
-#load "Lexing.fs"
-#load "./Parsing.fs"
-#load "./Parse.fs"
-
+#r "bin/Debug/netcoreapp3.0/MiniParser.dll"
 open MiniParser
 open System
 
 let parsed = [
     for x in (Environment.GetCommandLineArgs() |> List.ofArray |> List.skip 3) do
-    yield MiniParser.Parse.ParseString x ]
+    yield Parse.ParseString x ]
 
 parsed |> printfn "%A\n"
